b8e2be7262ac10cfe76e8fb49f5c5155701df624
oandrade <oandrade@oandrade-IdeaPad-U430-Touch>
2017-12-13 12:37:32 +0100

    Modif registerform

--- HTML5Application/public_html/js/controller.js
+++ HTML5Application/public_html/js/controller.js
@@ -6,6 +6,14 @@
 
 angular.module('StarD.Controllers',[])
         .controller('RootCtrl',function ($rootScope){})
-        .controller('RegisterCtrl',function (){});
+        .controller('RegisterCtrl',function ($http,$scope){
+            this.register = function($user){
+                window.alert("Register ok !");
+            };
+            this.reset = function(){
+                $scope.user = {};
+            };
+    
+        });
 
 
