801e5721fc2f8209a62c4a18ac82cb98a5e9b74c
1 <1@10.100.102.30>
2017-12-13 14:38:38 +0100

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/js/stard.js

--- HTML5Application/public_html/js/controller.js
+++ HTML5Application/public_html/js/controller.js
@@ -4,28 +4,28 @@
  * and open the template in the editor.
  */
 
-angular.module('StarD.Controllers',[])
-        .controller('RootCtrl',function ($rootScope){})
-        .controller('RegisterCtrl',function (){})
+angular.module('StarD.Controllers', [])
+        .controller('RootCtrl', function($rootScope) {})
+        .controller('RegisterCtrl', function() {})
 
-    .controller('DetailPlaneteCtrl',function ($scope,$http){
-        $scope.detail=[];
+        .controller('DetailPlaneteCtrl', function($scope, $http) {
+            $scope.detail = [];
 
-        $http.get('/HTML5Application/stars.json').then(function(reponse){
-            $scope.detail=reponse.data;
-        }, function(reponse){
-           window.alert("pas juste"); 
-        });
-    }) 
-    
-        .controller('RegisterCtrl',function ($http,$scope){
-            this.register = function($user){
+            $http.get('/HTML5Application/stars.json').then(function(reponse) {
+                $scope.detail = reponse.data;
+            }, function(reponse) {
+                window.alert("pas juste");
+            });
+        })
+
+        .controller('RegisterCtrl', function($http, $scope) {
+            this.register = function($user) {
                 window.alert("Register ok !");
             };
-            this.reset = function(){
+            this.reset = function() {
                 $scope.user = {};
             };
-    
+
         });
 
 
