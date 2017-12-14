e78daf7c6a34ff6a21c18381958cfc93b1169a47
1 <1@10.100.102.30>
2017-12-14 13:54:12 +0100

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/js/controller.js
    	HTML5Application/public_html/partials/detailPlanete.html

--- HTML5Application/public_html/js/controller.js
+++ HTML5Application/public_html/js/controller.js
@@ -4,38 +4,43 @@
  * and open the template in the editor.
  */
 
-angular.module('StarD.Controllers',[])
-        .controller('RootCtrl',function ($rootScope){})
-        .controller('RegisterCtrl',function (){})
+angular.module('StarD.Controllers', [])
+        .controller('RootCtrl', function($rootScope) {})
 
-    .controller('DetailPlaneteCtrl',function ($scope,$http){
-        $scope.detail=[];
+        .controller('DetailPlaneteCtrl', function($scope, $http) {
+            $scope.details = [];
 
-        $http.get('/HTML5Application/stars.json').then(function(reponse){
-            $scope.detail=reponse.data;
-        }, function(reponse){
-           window.alert("pas juste"); 
-        });
-    }) 
+            $http.get('/HTML5Application/stars.json').then(function(reponse) {
+                $scope.details = reponse.data;
+            }, function(reponse) {
+                window.alert("pas juste");
+            });
+        })
 
-    .controller('listePlaneteCtrl',function ($scope,$http){
-        $scope.detail=[];
 
-        $http.get('/HTML5Application/stars.json').then(function(reponse){
-            $scope.detail=reponse.data;
-        }, function(reponse){
-           window.alert("pas juste"); 
-        });
-    }) 
-    
-        .controller('RegisterCtrl',function ($http,$scope){
-            this.register = function($user){
+        .controller('ListePlaneteCtrl', function($scope, $rootScope, $http) {
+            this.sendPlanet = function() {
+                $rootScope.planete = $scope.detail;
+                alert($rootScope.planete);
+            };
+
+            $scope.details = [];
+
+            $http.get('/HTML5Application/stars.json').then(function(reponse) {
+                $scope.details = reponse.data;
+            }, function(reponse) {
+                window.alert("pas juste");
+            });
+        })
+
+        .controller('RegisterCtrl', function($http, $scope) {
+            this.register = function($user) {
                 window.alert("Register ok !");
             };
-            this.reset = function(){
+            this.reset = function() {
                 $scope.user = {};
             };
-    
+
         });
 
 
--- HTML5Application/public_html/js/stard.js
+++ HTML5Application/public_html/js/stard.js
@@ -4,9 +4,9 @@
  * and open the template in the editor.
  */
 
-var toto = angular.module('StarD', ['ngRoute']).config(['$routeProvider', function($routeProvider) {
+var toto = angular.module('StarD', ['ngRoute', 'StarD.Controllers']).config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/main', {templateUrl: 'partials/body.html'});
-        $routeProvider.when('/calculerDistance', {templateUrl: 'partials/calculerDistance.html', controller: 'CalculerDistCtrl as calculerDistCtrl'});
+        $routeProvider.when('/calculerDistance', {templateUrl: 'partials/calculerDistance.html', controller: 'ListePlaneteCtrl as listePlaneteCtrl'});
         $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl as registerCtrl'});
         $routeProvider.when('/detailPlanete', {templateUrl: 'partials/detailPlanete.html', controller: 'DetailPlaneteCtrl as detailPlaneteCtrl'});
         $routeProvider.otherwise({redirectTo: '/main'});
