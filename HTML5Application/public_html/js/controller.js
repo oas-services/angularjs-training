535bf3fc33d110733e2a35d4c95ed084693e45fb
1 <1@10.100.102.30>
2017-12-14 14:28:33 +0100

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/js/controller.js
    	HTML5Application/public_html/partials/detailPlanete.html

--- HTML5Application/public_html/js/controller.js
+++ HTML5Application/public_html/js/controller.js
@@ -8,24 +8,24 @@ angular.module('StarD.Controllers', [])
         .controller('RootCtrl', function($rootScope) {
             $rootScope.users = [{}];
         })
+
         .controller('DetailPlaneteCtrl', function($scope, $http) {
-            $scope.details = [];
+            $scope.detail = [];
 
             $http.get('/HTML5Application/stars.json').then(function(reponse) {
-                $scope.details = reponse.data;
+                $scope.detail = reponse.data;
             }, function(reponse) {
                 window.alert("pas juste");
             });
+
         })
 
-        .controller('ListePlaneteCtrl', function($scope, $rootScope, $http) {
-            this.sendPlanet = function() {
-                $rootScope.planete = $scope.detail;
-                alert($rootScope.planete);
-            };
-            $scope.details = [];
+
+        .controller('ListePlaneteCtrl', function($scope, $http) {
+            $scope.detail = [];
+
             $http.get('/HTML5Application/stars.json').then(function(reponse) {
-                $scope.details = reponse.data;
+                $scope.detail = reponse.data;
             }, function(reponse) {
                 window.alert("pas juste");
             });
